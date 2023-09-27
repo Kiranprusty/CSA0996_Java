de829a8344eb7ccdffdee34ce510d995e9e1c2bd
Kiranprusty <113409480+Kiranprusty@users.noreply.github.com>
2023-09-27 10:08:13 +0530

    Add files via upload

---
+++ day 1/programs/sortstring.java
@@ -0,0 +1,25 @@
+package day_1;
+import java.io.*;
+import java.util.*;
+public class sortstring {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 4;
+        String names[]= { "banana", "apple", "papaya", "orange" };
+        String temp;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (names[i].compareTo(names[j]) > 0) {
+                    temp = names[i];
+                    names[i] = names[j];
+                    names[j] = temp;
+                }
+            }
+        }
+        System.out.println("The names in alphabetical order are: ");
+        for (int i = 0; i < n; i++) {
+            System.out.println(names[i]);
+	}
+	}
+}
